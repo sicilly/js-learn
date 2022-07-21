e3dbc748bb0380d825e97a959cab37bf1b33eb44
琬姗 曹 <634068240@qq.com>
2022-07-21 13:50:53 +0800

    模块

---
+++ ES6/module/dist/js/m2.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+// 统一暴露
+var school = 'szu';
+
+function findJob() {
+    console.log("findjob");
+}
+
+exports.school = school;
+exports.findJob = findJob;
---
+++ ES6/module/dist/js/m3.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+// 默认暴露
+exports.default = {
+    school: 'szu',
+    change: function change() {
+        console.log("change");
+    }
+};
